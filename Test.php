9e0387461b5983d657cd97e253949a67d32935d8
eeyc88 <erick-yepez-cedeno@hotmail.com>
2017-03-05 16:42:32 -0500

    Fixed

--- Test.php
+++ Test.php
@@ -3,16 +3,9 @@
 class Test implements PHPUnit_Framework_TestCase
 {
 
-   public static function assertNotEquals($expected, $actual, $message = '', $delta = 0, $maxDepth = 10, $canonicalize = FALSE, $ignoreCase = FALSE)
-       {
-           $constraint = new PHPUnit_Framework_Constraint_Not(
-             new PHPUnit_Framework_Constraint_IsEqual(
-               $expected, $delta, $maxDepth, $canonicalize, $ignoreCase
-             )
-           );
-   
-           self::assertThat($actual, $constraint, $message);
-       }
+	public function testOnePlusOne() {
+		$this->assertEquals(1+1,2);
+  	}
 
 }
 
