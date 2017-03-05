a1f688f33776f3ca3c6effd36bf882445eeb1436
eeyc88 <erick-yepez-cedeno@hotmail.com>
2017-03-05 16:39:06 -0500

    fiexd

--- Test.php
+++ Test.php
@@ -3,11 +3,7 @@
 class Test implements PHPUnit_Framework_TestCase
 {
 
-  public $expected = 'Eduardo';
-  public $actual   = 'Erick';
-
-
-   public static function assertNotEquals($expected, $actual, $message = 'Excelente, ambas variables no son iguales', $delta = 0, $maxDepth = 10, $canonicalize = FALSE, $ignoreCase = FALSE)
+   public static function assertNotEquals($expected, $actual, $message = '', $delta = 0, $maxDepth = 10, $canonicalize = FALSE, $ignoreCase = FALSE)
        {
            $constraint = new PHPUnit_Framework_Constraint_Not(
              new PHPUnit_Framework_Constraint_IsEqual(
