c2875297045d245f68aad962e6aea8f06502729a
eeyc88 <erick-yepez-cedeno@hotmail.com>
2017-03-05 16:23:08 -0500

    refixeado

--- Test.php
+++ Test.php
@@ -1,9 +1,22 @@
 <?php
 
-class Test extends PHPUnit_Framework_TestCase
+class Test implements PHPUnit_Framework_TestCase
 {
 
-echo "Hola soy Erick";
+  public $expected = 'Eduardo';
+  public $actual   = 'Erick';
+
+
+   public static function assertNotEquals($expected, $actual, $message = 'Excelente, ambas variables no son iguales', $delta = 0, $maxDepth = 10, $canonicalize = FALSE, $ignoreCase = FALSE)
+       {
+           $constraint = new PHPUnit_Framework_Constraint_Not(
+             new PHPUnit_Framework_Constraint_IsEqual(
+               $expected, $delta, $maxDepth, $canonicalize, $ignoreCase
+             )
+           );
+   
+           self::assertThat($actual, $constraint, $message);
+       }
 
 }
 
